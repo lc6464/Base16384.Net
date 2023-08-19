4c7d604808b29fb0460de2ba65d87318d739bc08
lc6464 <64722907+lc6464@users.noreply.github.com>
2023-08-20 00:51:23 +0800

    似乎搬了 C 代码，但是并不能正常使用

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Base16384Coder;
+using System.Runtime.InteropServices;
+
 Console.WriteLine("Hello, World!");
 
 
 Console.Write("内容：");
-string text = Console.ReadLine() ?? "";
+var text = Console.ReadLine() ?? "";
 
 var data = Encoding.UTF8.GetBytes(text);
 
@@ -30,19 +33,36 @@ foreach (var one in data) {
 
 if (offset != 0) {
 	outputData.AddRange(BitConverter.GetBytes((short)(rest + 0x4e00)));
-	outputData.AddRange(BitConverter.GetBytes((short)(data.Length % 7 + 0x3d00)));
+	outputData.AddRange(BitConverter.GetBytes((short)((data.Length % 7) + 0x3d00)));
 	//output.Append(Encoding.Unicode.GetString(BitConverter.GetBytes((short)(rest + 0x4e00))));
 	//output.Append(Encoding.Unicode.GetString(BitConverter.GetBytes((short)(data.Length % 7 + 0x3d00))));
 }
 
-string outputStr = Encoding.Unicode.GetString(outputData.ToArray());
+var outputStr = Encoding.Unicode.GetString(outputData.ToArray());
 
 var e = DateTime.Now;
 //Console.WriteLine(outputStr);
 
 using var file = File.Create(@"test.txt");
-data = Encoding.Unicode.GetBytes(outputStr);
+data = Encoding.BigEndianUnicode.GetBytes(outputStr);
 file.Write(data, 0, data.Length);
 file.Flush();
 
 Console.WriteLine((e - s).TotalMilliseconds);
+
+
+s = DateTime.Now;
+
+unsafe {
+	var outPtr = (byte*)Marshal.AllocHGlobal(8192 * 1024 / 7 * 7);
+	var dataPtr = (byte*)Marshal.AllocHGlobal(data.Length);
+	Marshal.Copy(data, 0, (IntPtr)dataPtr, data.Length);
+	Base16384.Base16384_encode(dataPtr, data.Length, outPtr, 8192 * 1024 / 7 * 7);
+	var outData = new byte[8192 * 1024 / 7 * 7];
+	Marshal.Copy((IntPtr)outPtr, outData, 0, 8192 * 1024 / 7 * 7);
+	e = DateTime.Now;
+    using var file2 = File.Create(@"test1.txt");
+	file2.Write(outData, 0, 8192 * 1024 / 7 * 7);
+	file2.Flush();
+	Console.WriteLine((e - s).TotalMilliseconds);
+}
