c2eb75bd0079b337ab7b1a2c13bf195ac92e901b
lc6464 <64722907+lc6464@users.noreply.github.com>
2023-08-20 21:58:34 +0800

    从 C 版本修改而来。

--- Program.cs
+++ Program.cs
@@ -1,68 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Base16384Coder;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 
 Console.WriteLine("Hello, World!");
 
 
 Console.Write("内容：");
+
 var text = Console.ReadLine() ?? "";
 
 var data = Encoding.UTF8.GetBytes(text);
 
-//var data = File.ReadAllBytes(@"最终.png");
-
-//var data = Encoding.UTF8.GetBytes("这只是一段测试文本啦！啊哈哈哈！");
-
-int offset = 0, rest = 0;
-List<byte> outputData = new();
-
-var s = DateTime.Now;
-
-foreach (var one in data) {
-	offset += 8;
-	if (offset < 14) {
-		rest += (one << (14 - offset)) & 0x3fff;
-		continue;
-	}
-	offset -= 14;
-	outputData.AddRange(BitConverter.GetBytes((short)(rest + (one >> offset) + 0x4e00)));
-	//output.Append(Encoding.Unicode.GetString(BitConverter.GetBytes((short)(rest + (one >> offset) + 0x4e00))));
-	rest = (one << (14 - offset)) & 0x3fff;
-}
-
-if (offset != 0) {
-	outputData.AddRange(BitConverter.GetBytes((short)(rest + 0x4e00)));
-	outputData.AddRange(BitConverter.GetBytes((short)((data.Length % 7) + 0x3d00)));
-	//output.Append(Encoding.Unicode.GetString(BitConverter.GetBytes((short)(rest + 0x4e00))));
-	//output.Append(Encoding.Unicode.GetString(BitConverter.GetBytes((short)(data.Length % 7 + 0x3d00))));
-}
-
-var outputStr = Encoding.Unicode.GetString(outputData.ToArray());
-
-var e = DateTime.Now;
-//Console.WriteLine(outputStr);
-
-using var file = File.Create(@"test.txt");
-data = Encoding.BigEndianUnicode.GetBytes(outputStr);
-file.Write(data, 0, data.Length);
-file.Flush();
-
-Console.WriteLine((e - s).TotalMilliseconds);
-
-
-s = DateTime.Now;
+var u16bes = new byte[] { 0xfe, 0xff };
 
 unsafe {
-	var outPtr = (byte*)Marshal.AllocHGlobal(8192 * 1024 / 7 * 7);
+	var s = DateTime.Now;
+
 	var dataPtr = (byte*)Marshal.AllocHGlobal(data.Length);
 	Marshal.Copy(data, 0, (IntPtr)dataPtr, data.Length);
-	Base16384.Base16384_encode(dataPtr, data.Length, outPtr, 8192 * 1024 / 7 * 7);
-	var outData = new byte[8192 * 1024 / 7 * 7];
-	Marshal.Copy((IntPtr)outPtr, outData, 0, 8192 * 1024 / 7 * 7);
-	e = DateTime.Now;
-    using var file2 = File.Create(@"test1.txt");
-	file2.Write(outData, 0, 8192 * 1024 / 7 * 7);
-	file2.Flush();
+	var outPtr = (byte*)Marshal.AllocHGlobal(8192 * 1024 / 7 * 7);
+	var outData = new byte[Base16384.Base16384.Encode(dataPtr, data.Length, outPtr, 8192 * 1024 / 7 * 7)];
+	Marshal.Copy((IntPtr)outPtr, outData, 0, outData.Length);
+	Marshal.FreeHGlobal((IntPtr)outPtr);
+	Marshal.FreeHGlobal((IntPtr)dataPtr);
+
+	var e = DateTime.Now;
 	Console.WriteLine((e - s).TotalMilliseconds);
+
+	using var file2 = File.Create(@"D:\LC\Desktop\testCPtr.txt");
+	file2.Write(u16bes, 0, 2);
+	file2.Write(outData, 0, outData.Length);
+	file2.Flush();
+
+
+	s = DateTime.Now;
+
+	var decodingPtr = (byte*)Marshal.AllocHGlobal(outData.Length);
+	Marshal.Copy(outData, 0, (IntPtr)decodingPtr, outData.Length);
+	var decodedPtr = (byte*)Marshal.AllocHGlobal(8192 * 1024 / 7 * 7);
+	var decodedData = new byte[Base16384.Base16384.Decode(decodingPtr, outData.Length, decodedPtr, 8192 * 1024 / 7 * 7)];
+	Marshal.Copy((IntPtr)decodedPtr, decodedData, 0, decodedData.Length);
+	Marshal.FreeHGlobal((IntPtr)decodingPtr);
+	Marshal.FreeHGlobal((IntPtr)decodedPtr);
+
+	e = DateTime.Now;
+	Console.WriteLine((e - s).TotalMilliseconds);
+
+	File.WriteAllBytes(@"D:\LC\Desktop\testDecode.txt", decodedData);
 }
