7bdb67e5b846e679f9011b648cd21add1504298e
lc6464 <64722907+lc6464@users.noreply.github.com>
2023-08-23 21:22:33 +0800

    项目更名

--- Program.cs
+++ Program.cs
@@ -1,61 +1,72 @@
-﻿using System.Diagnostics;
+﻿if (args.Length == 1) {
+	if (args[0] == "e") {
+		DirectoryInfo directoryInfo = new("Source");
+		if (!directoryInfo.Exists) {
+			Console.WriteLine("无 Source 文件夹！");
+			return 1;
+		}
 
-DirectoryInfo directoryInfo = new("Source");
-if (!directoryInfo.Exists) {
-	Console.WriteLine("无 Source 文件夹！");
-	return 1;
-}
+		var files = directoryInfo.GetFiles();
+		if (files.Length == 0) {
+			Console.WriteLine("Source 文件夹为空！");
+			return 2;
+		}
 
-var files = directoryInfo.GetFiles();
-if (files.Length == 0) {
-	Console.WriteLine("Source 文件夹为空！");
-	return 2;
-}
+		if (!Directory.Exists("Result")) {
+			if (File.Exists("Result")) {
+				Console.WriteLine("Result 存在但为文件！");
+				return 3;
+			}
+			Directory.CreateDirectory("Result");
+		}
 
-if (!Directory.Exists("Result")) {
-	if (File.Exists("Result")) {
-		Console.WriteLine("Result 存在但为文件！");
-		return 3;
+		foreach (var file in files) {
+			FileInfo encodedFileInfo = new(@$"Result\{file.Name}.Encoded");
+
+			Base16384.EncodeFromFileToNewFile(file, encodedFileInfo);
+
+			Console.WriteLine($"{file.Name} - Finished.");
+		}
+
+		Console.WriteLine("\nAll done.");
+	} else if (args[0] == "d") {
+		DirectoryInfo directoryInfo = new("Encoded");
+		if (!directoryInfo.Exists) {
+			Console.WriteLine("无 Encoded 文件夹！");
+			return 1;
+		}
+
+		var files = directoryInfo.GetFiles();
+		if (files.Length == 0) {
+			Console.WriteLine("Encoded 文件夹为空！");
+			return 2;
+		}
+
+		if (!Directory.Exists("Result")) {
+			if (File.Exists("Result")) {
+				Console.WriteLine("Result 存在但为文件！");
+				return 3;
+			}
+			Directory.CreateDirectory("Result");
+		}
+
+		foreach (var file in files) {
+			FileInfo decodedFileInfo = new(@$"Result\{file.Name}.Decoded");
+
+			Base16384.DecodeFromFileToNewFile(file, decodedFileInfo);
+
+			Console.WriteLine($"{file.Name} - Finished.");
+		}
+
+		Console.WriteLine("\nAll done.");
+	} else {
+		Console.WriteLine("Unknown argument.");
+		return 4;
 	}
-	Directory.CreateDirectory("Result");
+	return 0;
 }
 
-foreach (var file in files) {
-	FileInfo encodedFileInfo = new(@$"Result\{file.Name}.Encode"),
-		decodedFileInfo = new(@$"Result\{file.Name}.Decode");
 
-	Base16384.EncodeFromFileToNewFile(file, encodedFileInfo);
-	Base16384.DecodeFromFileToNewFile(encodedFileInfo, decodedFileInfo);
+Console.WriteLine("Unknown argument.");
 
-	using var encP = new Process();
-	encP.StartInfo.FileName = "base16384.com"; // skipcq: CS-S1002 临时调试用，让系统从 PATH 中寻找
-	encP.StartInfo.Arguments = $"-e \"{file.FullName}\" \"{encodedFileInfo.FullName}.Raw\"";
-	encP.StartInfo.RedirectStandardOutput = true;
-	encP.Start();
-	encP.WaitForExit(5000);
-	var encS = encP.StandardOutput.ReadToEnd();
-	if (!string.IsNullOrWhiteSpace(encS)) {
-		Console.WriteLine($"{file.Name} - encR - {encS}");
-	} else if (encP.ExitCode != 0) {
-		Console.WriteLine($"{file.Name} - encR - {encP.ExitCode}");
-	}
-
-	using var decP = new Process();
-	decP.StartInfo.FileName = "base16384.com"; // skipcq: CS-S1002 临时调试用，让系统从 PATH 中寻找
-	decP.StartInfo.Arguments = $"-d \"{encodedFileInfo.FullName}.Raw\" \"{decodedFileInfo.FullName}.Raw\"";
-	decP.StartInfo.RedirectStandardOutput = true;
-	decP.Start();
-	decP.WaitForExit(5000);
-	var decS = decP.StandardOutput.ReadToEnd();
-	if (!string.IsNullOrWhiteSpace(decS)) {
-		Console.WriteLine($"{file.Name} - decR - {decS}");
-	} else if (decP.ExitCode != 0) {
-		Console.WriteLine($"{file.Name} - encR - {decP.ExitCode}");
-	}
-
-	Console.WriteLine($"{file.Name} - Finished.");
-}
-
-Console.WriteLine("\nAll done.");
-
-return 0;
+return 4;
