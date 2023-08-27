cce7d22bd9279219d6fd7963315342c495516ec6
lc6464 <64722907+lc6464@users.noreply.github.com>
2023-08-27 22:42:15 +0800

    测试代码

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿using System.Diagnostics; { // 编码
+﻿using System.Text;
+using System.Diagnostics;
+
+Span<byte> buffer = new byte[8388650];
+Span<byte> codingBuffer = new byte[Math.Max(Base16384.EncodeLength(Base16384.Buffer0Length), Base16384.DecodeLength(Base16384.Buffer1Length)) + 16]; /*{ // 编码
 	DirectoryInfo directoryInfo = new("Source");
 	if (!directoryInfo.Exists) {
 		Console.WriteLine("无 Source 文件夹！");
@@ -22,8 +26,8 @@
 	foreach (var file in files) {
 		FileInfo encodedFileInfo = new(Path.Combine("Result", $"{file.Name}.Encoded"));
 
-		Base16384.EncodeFromFileToNewFile(file, encodedFileInfo);
-		ProcessByC.Encode(file, new(Path.Combine("Result", $"{file.Name}")));
+		Base16384.EncodeFromFileToNewFile(file, encodedFileInfo, buffer, codingBuffer);
+		//ProcessByC.Encode(file, new(Path.Combine("Result", $"{file.Name}")));
 
 		Console.WriteLine($"{file.Name} - Finished.");
 	}
@@ -53,8 +57,8 @@
 	foreach (var file in files) {
 		FileInfo decodedFileInfo = new(Path.Combine("Result", $"{file.Name}.Decoded"));
 
-		Base16384.DecodeFromFileToNewFile(file, decodedFileInfo);
-		ProcessByC.Decode(file, new(Path.Combine("Result", $"{file.Name}")));
+		Base16384.DecodeFromFileToNewFile(file, decodedFileInfo, buffer, codingBuffer);
+		//ProcessByC.Decode(file, new(Path.Combine("Result", $"{file.Name}")));
 
 		Console.WriteLine($"{file.Name} - Finished.");
 	}
@@ -65,6 +69,20 @@
 foreach (var file in new DirectoryInfo("Source").GetFiles()) {
 	file.CopyTo(Path.Combine("Result", file.Name), true);
 }
+*/
+
+DirectoryInfo directoryInfo = new("Source");
+
+var fileInfo = directoryInfo.GetFiles()[0];
+
+var stream = new MemoryStream();
+
+Base16384.EncodeFromFileToStream(fileInfo, stream, buffer, codingBuffer);
+
+ReadOnlySpan<byte> data = stream.ToArray();
+
+using var file = File.Create(@"D:\LC\Desktop\test.txt");
+file.Write(data.ConvertFromUtf16BEBytesToUtf8BOMBytes());
 
 return 0;
 
