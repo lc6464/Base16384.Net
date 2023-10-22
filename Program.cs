494fcdb76febb23bc5a10bf120e5c3e9acc3cfdb
execute233 <1647643661@qq.com>
2023-10-22 12:10:10 +0800

    -

--- Program.cs
+++ Program.cs
@@ -1,22 +1,33 @@
-﻿if (args.Length <= 3) {
-	switch (args[0]) {
-		case "e":
-			if (args.Length < 2) {
-				Console.WriteLine("未指定文件或目录");
-				return 5;
-			}
-			return Directory.Exists(args[1]) ? EncodeOrDecodeDirectory(args[1], args.Length == 3 ? args[2] : Path.Combine(args[1], "out"), true) : EncodeOrDecodeFromFileToFile(args[1], args.Length == 3 ? args[2] : $"{args[1]}.encoded", true);
-		case "d":
-			if (args.Length < 2) {
-				Console.WriteLine("未指定文件或目录");
-				return 5;
-			}
-			return Directory.Exists(args[1]) ? EncodeOrDecodeDirectory(args[1], args.Length == 3 ? args[2] : Path.Combine(args[1], "out"), false) : EncodeOrDecodeFromFileToFile(args[1], args.Length == 3 ? args[2] : $"{args[1]}.encoded", false);
-		default:
-			Console.WriteLine("Unknown argument.");
-			return 4;
-	}
+﻿switch (args.Length)
+{
+	case 0:
+		Console.WriteLine("用法: Base16384.exe <e|d> <input | \"-\"> [output | \"-\"]");
+		Console.WriteLine("e: Encode，将指定 文件/目录下的文件编码至 Result 目录下。");
+		Console.WriteLine("d: Decode，将 文件/目录下的文件解码至 Result 目录下。");
+		Console.WriteLine("source: 源文件路径，可以是文件夹，也可以是文件。");
+		Console.WriteLine("out: 输出文件路径，可以是文件夹，也可以是文件。");
+		Console.WriteLine("\"-\" 代表 Standard IO。");
+		return 4;
+	case <= 3:
+		switch (args[0]) {
+			case "e":
+				if (args.Length < 2) {
+					Console.WriteLine("未指定文件或目录");
+					return 5;
+				}
+				return Directory.Exists(args[1]) ? EncodeOrDecodeDirectory(args[1], args.Length == 3 ? args[2] : Path.Combine(args[1], "out"), true) : EncodeOrDecodeFromFileToFile(args[1], args.Length == 3 ? args[2] : $"{args[1]}.encoded", true);
+			case "d":
+				if (args.Length < 2) {
+					Console.WriteLine("未指定文件或目录");
+					return 5;
+				}
+				return Directory.Exists(args[1]) ? EncodeOrDecodeDirectory(args[1], args.Length == 3 ? args[2] : Path.Combine(args[1], "out"), false) : EncodeOrDecodeFromFileToFile(args[1], args.Length == 3 ? args[2] : $"{args[1]}.decoded", false);
+			default:
+				Console.WriteLine("Unknown argument.");
+				return 4;
+		}
 }
+
 Console.WriteLine("Unknown argument.");
 return 4;
 // mode: true为encode，false为decode
@@ -46,6 +57,10 @@ int EncodeOrDecodeDirectory(string inputPath, string outputPath, bool mode) {
 }
 // mode: true为encode，false为decode
 int EncodeOrDecodeFromFileToFile(string inputPath, string outputPath, bool mode) {
+	if (Directory.Exists(outputPath)) {
+		outputPath = Path.Combine(outputPath, $"{new FileInfo(inputPath).Name}.{(mode ? "en" : "de")}coded");
+	}
+	
 	const string minus = "-";
 	bool inputIsStd = inputPath == minus;
 	bool outputIsStd = outputPath == minus;
@@ -71,6 +86,7 @@ bool DirectoryExistsButFileNorCreate(string path) {
 	if (File.Exists(path)) {
 		return true;
 	}
+	Console.WriteLine($"自动创建{path}目录");
 	Directory.CreateDirectory(path);
 	return false;
 }
--- README.md
+++ README.md
@@ -6,24 +6,13 @@
 此项目引用实现后的库，作为控制台应用程序供用户使用。
 
 ## 使用方法
-### Tree
-```
-|-- Base16384.Net.exe
-|-- Base16384.Net.dll
-|-- Source
-	|-- xxx.jpg
-	|-- xxx.png
-|-- Encoded
-	|-- xxx.jpg.Encoded
-	|-- xxx.png.Encoded
-```
 ### Command
 ```
-Base16384.Net.exe <e|d>
+Base16384.Net.exe <"e" | "d"> <source | "-"> [out | "-"]
 ```
-- e: Encode，将 Source 目录下的文件编码至 Result 目录下
-- d: Decode，将 Encoded 目录下的文件解码至 Result 目录下
-
-## 另一种使用方法
-
-成熟的程序员应该自己看代码来发现使用方法。
+- e: Encode，将指定 文件/目录下的文件编码至 Result 目录下。
+- d: Decode，将 文件/目录下的文件解码至 Result 目录下。
+- source: 源文件路径，可以是文件夹，也可以是文件。
+- out: 输出文件路径，可以是文件夹，也可以是文件。
+- "-" 代表 Standard IO。
+#### 未指定输出路径或输入为文件而输出为文件夹时时，默认输出到该目录 源文件名.encoded/decoded,输入为文件夹但输出未指定文件夹时，输出到同级文件夹out
