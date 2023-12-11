a146eca2f568b6546023fcdd5efecf20cdf8cd3a
lc6464 <64722907+lc6464@users.noreply.github.com>
2023-12-11 23:47:50 +0800

    完善已有逻辑的几乎所有异常处理和用户提示

--- Program.cs
+++ Program.cs
@@ -189,87 +189,18 @@ if (args[0] == "e") {
 				Console.WriteLine("Source file not found.");
 				return 2;
 			}
-			Base16384.DecodeFromFileToNewFile(new(args[1]), new(args[2]));
+			FileInfo info1 = new(args[1]),
+				info2 = new(args[2]);
+			Console.Write($"{info1.Name} -> {info2.Name} ... ");
+			try {
+				Base16384.DecodeFromFileToNewFile(info1, info2);
+			} catch {
+				Console.WriteLine("Failed.");
+				return 4;
+			}
+			Console.WriteLine("Done.");
 		}
 	}
 }
-
 
 return 0;
-
-
-/*
-if (args.Length == 1) {
-	if (args[0] == "e") {
-		DirectoryInfo directoryInfo = new("Source");
-		if (!directoryInfo.Exists) {
-			Console.WriteLine("无 Source 文件夹！");
-			return 1;
-		}
-
-		var files = directoryInfo.GetFiles();
-		if (files.Length == 0) {
-			Console.WriteLine("Source 文件夹为空！");
-			return 2;
-		}
-
-		if (!Directory.Exists("Result")) {
-			if (File.Exists("Result")) {
-				Console.WriteLine("Result 存在但为文件！");
-				return 3;
-			}
-			Directory.CreateDirectory("Result");
-		}
-
-		foreach (var file in files) {
-			FileInfo encodedFileInfo = new(Path.Combine("Result", $"{file.Name}.Encoded"));
-
-			Base16384.EncodeFromFileToNewFile(file, encodedFileInfo);
-
-			Console.WriteLine($"{file.Name} - Finished.");
-		}
-
-		Console.WriteLine("\nAll done.");
-	} else if (args[0] == "d") {
-		DirectoryInfo directoryInfo = new("Encoded");
-		if (!directoryInfo.Exists) {
-			Console.WriteLine("无 Encoded 文件夹！");
-			return 1;
-		}
-
-		var files = directoryInfo.GetFiles();
-		if (files.Length == 0) {
-			Console.WriteLine("Encoded 文件夹为空！");
-			return 2;
-		}
-
-		if (!Directory.Exists("Result")) {
-			if (File.Exists("Result")) {
-				Console.WriteLine("Result 存在但为文件！");
-				return 3;
-			}
-			Directory.CreateDirectory("Result");
-		}
-
-		foreach (var file in files) {
-			FileInfo decodedFileInfo = new(Path.Combine("Result", $"{file.Name}.Decoded"));
-
-			Base16384.DecodeFromFileToNewFile(file, decodedFileInfo);
-
-			Console.WriteLine($"{file.Name} - Finished.");
-		}
-
-		Console.WriteLine("\nAll done.");
-	} else {
-		Console.WriteLine("Unknown argument.");
-		return 4;
-	}
-	return 0;
-}
-
-
-Console.WriteLine("Unknown argument.");
-
-return 4;
-
-*/
